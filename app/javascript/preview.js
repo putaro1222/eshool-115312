7f01d6b13a6b042d8315b2a2c37db0e58092c03d
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-05-25 00:29:17 +0900

    プレビュー機能

---
+++ app/javascript/preview.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list');
+
+  document.getElementById('curriculums-image').addEventListener('change', function(e){
+    console.log(e);
+  });
+});
